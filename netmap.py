a1e41cd5c1ec9c0e695457c170cb24e4be478ead
reb <reb@FURBIFY-BDBSHKB.home>
2026-01-29 17:14:41 +0100

    simple input

--- netmap.py
+++ netmap.py
@@ -4,7 +4,10 @@ import scapy.all as scapy
 
 def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
-    print(arp_request.summary())
-    scapy.ls(scapy.ARP())
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast = broadcast/arp_request
+    answered, unanswered = scapy.srp(arp_request_broadcast, timeout=1)
+    print(answered.summary())
 
-scan("192.168.1.1/24")
+ip = raw_input("Enter IP to scan: ")
+scan(ip)
