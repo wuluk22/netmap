5759c783ad7be7a5e27caf18dc9b3ec80f7267b7
reb <reb@FURBIFY-BDBSHKB.home>
2026-01-29 17:00:15 +0100

    simple APR request

--- netmap.py
+++ netmap.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python
+
+import scapy.all as scapy
+
+def scan(ip):
+    arp_request = scapy.ARP(pdst=ip)
+    print(arp_request.summary())
+    scapy.ls(scapy.ARP())
+
+scan("192.168.1.1/24")
