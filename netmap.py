204238722e3bb407336c6e05df8778b6b64292ab
reb <reb@FURBIFY-BDBSHKB.home>
2026-01-29 18:13:20 +0100

    passed to argparse since optparse is deprecated

--- netmap.py
+++ netmap.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 
 import scapy.all as scapy
-import optparse
+import argparse
 
 def get_arguments():
-    parser = optparse.OptionParser()
-    parser.add_option("-t", "--target", dest="target", help="Target IP address / IP range")
-    options, args = parser.parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-t", "--target", dest="target", help="Target IP address / IP range")
+    options = parser.parse_args()
     if not options.target:
         parser.error("[-] Please specify a target IP address")
     return options
