0b4c0cee7f980f3985b20163507fbfdaa597ad23
reb <reb@FURBIFY-BDBSHKB.home>
2026-01-29 17:28:13 +0100

    only shows IP and MAC adress of chosen targeted IP or subnet

--- netmap.py
+++ netmap.py
@@ -6,8 +6,11 @@ def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_request_broadcast = broadcast/arp_request
-    answered, unanswered = scapy.srp(arp_request_broadcast, timeout=1)
-    print(answered.summary())
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1)[0]
+
+    for element in answered_list:
+        print(element[1].psrc)
+        print(element[1].hwsrc)
 
 ip = raw_input("Enter IP to scan: ")
 scan(ip)
